19c089afb88e4a97df3793f7c8f6510577ca3a0f
Andre Machado <csantos.machado@gmail.com>
2019-02-26 17:15:40 -0300

    Initial commit.!

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Initialize basic vars and helpers
+source blueprint-lib/init.sh
+
+##
+# Add dependencies here
+##
+
+# source blueprint-lib/docker.sh
+
+##
+# ADD BLUEPRINT CODE BELOW HERE
+#
+# BASE_PATH is the full path to the project root
+# APP_NAME is the name of the Django app that will be modified
+##
+rm -rf $BASE_PATH/*
+rm -rf $BASE_PATH/.*
+
+cp blueprint-lib/content/* $BASE_PATH/
